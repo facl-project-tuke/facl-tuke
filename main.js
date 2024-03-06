e32090dba4ad2940113e1d0a1eaf556236bd74e6
Иван Куличев <ivankulicev@ivankulichev508.intrak.tuke.sk>
2024-03-06 16:48:24 +0100

    asdasd

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ const homeBtn = document.querySelector(".navigation__item_home");
 const introContent = document.querySelector(".intro");
 const mainContent = document.querySelector(".main");
 
+const introSection = document.querySelector(".intro");
+const speed = 0.5; // Скорость параллакс эффекта. Можно изменять для разного уровня эффекта.
+const offset = window.pageYOffset * speed;
+
+// Изменяем позицию фонового изображения секции intro
+introSection.style.backgroundPosition = "center " + (offset - 50) + "px";
+
 aboutBtn.addEventListener("click", () => {
   homeBtn.classList.remove("navigation__item--active");
   aboutBtn.classList.add("navigation__item--active");
   introContent.style.display = "none";
   mainContent.style.display = "none";
-  document.querySelector(".container_cards").style.display = "flex"
+  document.querySelector(".container_cards").style.display = "flex";
 });
 
 homeBtn.addEventListener("click", () => {
@@ -18,5 +25,23 @@ homeBtn.addEventListener("click", () => {
   homeBtn.classList.add("navigation__item--active");
   introContent.style.display = "block";
   mainContent.style.display = "block";
-  document.querySelector(".container_cards").style.display = "none"
+  document.querySelector(".container_cards").style.display = "none";
+});
+
+document.addEventListener("scroll", function () {
+  const introSection = document.querySelector(".intro");
+  const speed = 0.5; // Скорость параллакс эффекта. Можно изменять для разного уровня эффекта.
+  const offset = window.pageYOffset * speed;
+
+  // Изменяем позицию фонового изображения секции intro
+  introSection.style.backgroundPosition = "center " + (offset - 50) + "px";
+});
+
+document.getElementById("downloadPdfBtn").addEventListener("click", () => {
+  let link = document.createElement("a");
+  link.href = "./01 - Enhancing Learning Efficiency in FACL -- A Novel Fuzzy Rule Transfer Method for Transfer Learning - Ni (2024).pdf"; // Замените на актуальный путь к файлу
+  link.download = "Enhancing Learning Efficiency in FACL -- A Novel Fuzzy Rule Transfer Method for Transfer Learning - Ni (2024).pdf"; // Необязательный атрибут, задаёт имя скачиваемого файла
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 });
